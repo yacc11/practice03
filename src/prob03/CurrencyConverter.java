86326e4968df4b5a68de51469a169f2ce7b6925a
bit-user <bit-user@bit>
2017-04-28 17:05:04 +0900

    환율 매개변수 변경.

--- src/prob03/CurrencyConverter.java
+++ src/prob03/CurrencyConverter.java
@@ -3,7 +3,6 @@ package prob03;
 public class CurrencyConverter {
 
 	private static double rate;
-	private static double rate1; //void setRate끝나면 환율이 사라지므로 새로운 변수 생성 
 	
 	public static double toDollar(double won) { //1달러에 1197원 
 		// 한국 원화를 달러로 변환
@@ -12,8 +11,8 @@ public class CurrencyConverter {
 		//환율 1197
 		//1197x = 1000000
 		//x= 1000000/1197
-		//System.out.println(rate);
-		won=won/rate1;
+
+		won=won/rate;
 		return won;
 	}
 	
@@ -21,12 +20,14 @@ public class CurrencyConverter {
 		// 달러를 한국 원화로 변환
 		//100달러
 		//1 : 1197 = 100 : x
-		dollar = dollar*rate1;
+		dollar = dollar*rate;
 		return dollar;
 	}
-	public static void setRate(double rate) {
+	public static void setRate(double r) {
 		// 환율 설정(KRW/$1)
-		rate1=rate;
+		rate=r;
+		
+		//rate1=rate;
 		
 	}
 }
